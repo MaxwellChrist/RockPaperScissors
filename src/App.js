5b5a1fb54d0953963b5fdfd08a095099b8c9484f
MaxwellChrist <ribthulu@gmail.com>
2022-09-28 12:39:17 -0400

    linked html to css, deleted unnecessary files and got react set up successfully

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world</h1>
+        Score
+        Rules
+
+        You Picked
+        The House Picked
+
+        You Win
+        You Lose
+
+        Play Again
+      </header>
+    </div>
+  );
+}
+
+export default App;
