b182c323ffb9ca78b89b47a529f854c78b8106fd
MaxwellChrist <ribthulu@gmail.com>
2022-09-29 09:33:31 -0400

    setting up game display component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import GameDisplay from './Components/GameDisplay';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <GameDisplay />
         Score
         Rules
 
---
+++ src/Components/GameDisplay.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './GameDisplay.css'
+
+export default function GameDisplay() {
+    return (
+        <>
+            <div className="display-container">
+                <div className="symbol-background rock-background">
+                    <div className="rock symbol">
+                        <img src="./images/icon-rock.svg" alt="faded lines connecting game icons" />
+                    </div>
+                </div>
+                <div className="symbol-background paper-background">
+                    <div className="paper symbol">
+                        <img src="./images/icon-paper.svg" alt="faded lines connecting game icons" />
+                    </div>
+                </div>
+                <div className="symbol-background scissors-background">
+                    <div className="scissors symbol">
+                        <img src="./images/icon-scissors.svg" alt="faded lines connecting game icons" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
